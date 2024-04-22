c6b850791ef6968e0e1932444e2c01e5891dbd9c
02snehil <snehil2024@gmail.com>
2024-04-22 17:53:20 +0530

    edit animation part

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,44 +1,70 @@
-import React from "react";
-import './Section2.css'
+import React, { useEffect } from "react";
+import './Section2.css';
 import icons1 from './image/icons1.png'
 import icons2 from './image/icons2.png'
 
-
 function Section2(){
+
+    useEffect(() => {
+        const img1 = document.querySelector('.leftToRight');
+        const img2 = document.querySelector('.rightToLeft');
+        let animationCount1 = 0;
+        let animationCount2 = 0;
+
+        const handleAnimationIteration = (event) => {
+            if (event.animationName === 'moveLeftToRight') {
+                animationCount1++;
+                if (animationCount1 >= 3) {
+                    img1.style.animation = 'none';
+                }
+            } else if (event.animationName === 'moveRightToLeft') {
+                animationCount2++;
+                if (animationCount2 >= 3) {
+                    img2.style.animation = 'none';
+                }
+            }
+        };
+        
+        img1.addEventListener('animationiteration', handleAnimationIteration);
+        img2.addEventListener('animationiteration', handleAnimationIteration);
+        
+        return () => {
+            img1.removeEventListener('animationiteration', handleAnimationIteration);
+            img2.removeEventListener('animationiteration', handleAnimationIteration);
+        };
+    }, []);
 
     return(
         <> 
-           <div className="container2">
-             <div className="heading2">
-                <h1>
-                    <br />Become <br /> the Top <br /> 1%              
-                </h1>
-                <h4>
-                  Whether it is Product, Growth, Design, 
-                  Management, Business, Tech & Data  
-                  GrowthSchool is the place to learn from top  
-                  experts in the field.
-                </h4>
-                <button className="explore_programs">
-                  <b>Explore Programs</b>
-                </button>
-             </div>
-             <div className="paragraph">
-                 {/* <p> and many more who are working at  <br />
-                     some of the top companies
-                 </p> */}
-             </div>
-             <div className="images">
-               <img src={icons1} alt="icons1" className="leftToRight" />
-             </div>
-             <div className="images">
-               <img src={icons2} alt="icons2" className="rightToLeft" />
-             </div>
-
-
-
-           </div>
+            <div className="container2">
+                <div className="heading2">
+                    <h1>
+                        <br />Become <br /> the Top <br /> 1%              
+                    </h1>
+                    <h4>
+                        Whether it is Product, Growth, Design, 
+                        Management, Business, Tech & Data  
+                        GrowthSchool is the place to learn from top  
+                        experts in the field.
+                    </h4>
+                    <button className="explore_programs">
+                        <b>Explore Programs</b>
+                    </button>
+                </div>
+                <div className="paragraph">
+                    {/* <p> and many more who are working at  <br />
+                        some of the top companies
+                    </p> */}
+                </div>
+                <div className="images">
+                    <img src={icons1} alt="icons1" className="leftToRight" />
+                </div>
+                <div className="images">
+                    <img src={icons2} alt="icons2" className="rightToLeft" />
+                </div>
+            </div>
         </>
     )
 }
+
 export default Section2;
--- src/components/Section9.js
+++ src/components/Section9.js
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Section9.css';
-import img9 from './image/img9.png'
+import img9 from './image/img9.png';
 
-function Section9(){
+function Section9() {
+    const [animationCount, setAnimationCount] = useState(0);
 
-    return(
-        <>
-            <div className="container9">
-                <div className="heading9">
-                    <p>We are trusted by the best!</p>
-                </div>  
-                <div className="boxes9">
-                    <div className="box9_1">
-                        <p>Better</p>
-                    </div>
-                    <div className="box9_2">
-                        <p>640 OXFORD VENTURES</p>
-                    </div>
-                </div>   
-                <div className="img9">
-                      <img src={img9} alt="img9" />
-                </div>    
+    useEffect(() => {
+        const imgElement = document.querySelector('.img9 img');
+        const handleAnimationEnd = () => {
+            setAnimationCount(prevCount => prevCount + 1);
+        };
+
+        imgElement.addEventListener('animationiteration', handleAnimationEnd);
+
+        return () => {
+            imgElement.removeEventListener('animationiteration', handleAnimationEnd);
+        };
+    }, []);
+
+    useEffect(() => {
+        const imgElement = document.querySelector('.img9 img');
+        if (animationCount >= 3) {
+            imgElement.classList.remove('animate');
+        }
+    }, [animationCount]);
+
+    return (
+        <div className="container9">
+            <div className="heading9">
+                <p>We are trusted by the best!</p>
             </div>
-        </>
+            <div className="boxes9">
+                <div className="box9_1">
+                    <p>Better</p>
+                </div>
+                <div className="box9_2">
+                    <p>640 OXFORD VENTURES</p>
+                </div>
+            </div>
+            <div className="img9">
+                <img src={img9} alt="img9" className="animate" />
+            </div>
+        </div>
     );
 }
+
 export default Section9;
